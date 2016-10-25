6c1e64012f4d3e560ab6327414fb4d3cde1f36a4
kim.jinnhonn <dnvy0084@gmail.com>
2016-10-25 14:09:39 +0900

    call, cwrap sample 작성 중.

--- 01.ccall_cwrap/src/ccall_cwrap.cpp
+++ 01.ccall_cwrap/src/ccall_cwrap.cpp
@@ -1,13 +1,27 @@
 
-#include <stdio>
+#include <stdio.h>
 
 extern "C"{
 
-	int getN(){
-		return 3;
+	int sumWithConst( int );
+	int sumAandB( int, int );
+
+	const int n = 3;
+
+	// js로 컴파일 시 접두어로 항상 문자열 "_"가 붙는다. 
+	// EXPORTED_FUNCTIONS로 지정하지 않은 함수도 호출 가능하다. 
+	int sumWithConst( int a ){
+
+		return sumAandB( a, n );
 	}
 
-	int sum( a ){
-		return a + getN();
+	int sumAandB( int a, int b ){
+
+		return a + b;
+	}
+
+	void print( char * str ){
+
+		printf("%s\n", str );
 	}
 }
